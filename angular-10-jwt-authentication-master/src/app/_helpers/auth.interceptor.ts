e63f94fa0a47101ce3dc5864b0a9030711ebd7e5
NizarNasraoui1 <108674818+NizarNasraoui1@users.noreply.github.com>
2022-09-05 00:46:26 +0200

    Update auth.interceptor.ts
    fix interceptor

--- angular-10-jwt-authentication-master/src/app/_helpers/auth.interceptor.ts
+++ angular-10-jwt-authentication-master/src/app/_helpers/auth.interceptor.ts
@@ -13,7 +13,6 @@ export class AuthInterceptor implements HttpInterceptor {
   constructor(private token: TokenStorageService) { }
 
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-    let authReq = req;
     const token = this.token.getToken();
     if (token != null) {
       // for Spring Boot back-end
